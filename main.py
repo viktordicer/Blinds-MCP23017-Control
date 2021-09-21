5056f2608ef65dacd97b37860af546da89e66a4a
viktordicer <viktor.dicer@gmail.com>
2021-09-21 21:41:54 +0200

    add calibrate function

--- main.py
+++ main.py
@@ -24,24 +24,29 @@ All blinds connect to the expand modules MCP23017
     6 - Child1 
     7 - Child2
     8 - Child3
+    
+    MQTT command format f-100-11111-111
+    f- full or t - tilt
+    100 - closed is 0, opened is 100
+    11111 - 5bits for first MCP23017 device with I2C address 0x20 last number is for first blind-(00001)
+    111 - 3bits - for second MCP23017 device with I2C address 0x21 last number is for sixth blind-(001)
+    
 """
-
-
-i2c_addr = [0x20, 0x21] #I2C address for devices
-i2c_register_out = [0x00, 0x01] #Set A and B to output
-i2c_register = [0x12, 0x13] # 0x012 is A register and 0x13 is B register
-motor_delay = 0.4 # delay between stop and command for motors
 
 Broker = "192.168.0.121"
 sub_topic = ["sensor/blind/all"] 
 
+i2c_addr = [0x20, 0x21] #I2C address for devices
+i2c_register_out = [0x00, 0x01] #Set A and B to output
+i2c_register = [0x12, 0x13] # 0x012 is A register and 0x13 is B register
+motor_delay = 0.5 # delay between stop and command for motors
+calibration_delay = 120 # all blinds go down
 
-bus = smbus.SMBus(1)
 
 blinds = [] # list of objects all blinds
 #Create new objects of blinds
 #dev1
-living = bld.Blind(42.7,42.3,1.2,0,1)
+living = bld.Blind(42.7,42.3,1.2,0,1)  # (full open time, full close time , tilt time, number of device, bit of blind in integer)
 kitchen = bld.Blind(42.7,42.3,1.2,0,2)
 terrace = bld.Blind(42.7,42.3,1.2,0,4)
 bed     = bld.Blind(42.7,42.3,1.2,0,8)
@@ -64,26 +69,29 @@ blinds.append(child2)
 blinds.append(child3)
 
 
-##	SET REGISTER AS OUTPUTS AND CLEAR ALL REGISTERS
-for dev in i2c_addr:
-    for reg_out in i2c_register_out:
-        bus.write_byte_data(dev,reg_out,0x00) # Set all of bank A and B to outputs 
-    for reg in i2c_register:
-        bus.write_byte_data(dev,reg,0) # Clear register A and B
-
-
+# thread function for auto stop
 def blind_ctr():
     while 1:
         for i in blinds:
             time_now = time.time()
             if i.movement != 'stop':
                 if time_now - i.last_run > i.duration:
-                    reg_A = read_data(i2c_addr[i.device], i2c_register[0])
+                    reg_A = read_data(i2c_addr[i.device], i2c_register[0]) #
                     reg_B = read_data(i2c_addr[i.device], i2c_register[1])
                     write_data(i2c_addr[i.device],i2c_register[0],bld.clear_bit(reg_A,int(i.bit))) #clear particular bit
                     write_data(i2c_addr[i.device],i2c_register[1],bld.clear_bit(reg_B,int(i.bit)))
                     i.stop()
                     print(i.position)
+
+def set_reg_as_output():
+    for dev in i2c_addr:
+        for reg_out in i2c_register_out:
+            bus.write_byte_data(dev,reg_out,0x00)
+
+def clear_register():
+    for dev in i2c_addr:
+        for reg in i2c_register:
+            bus.write_byte_data(dev,reg,0) # Clear register A and B
 
 #read data from register
 def read_data(device,register):
@@ -101,32 +109,45 @@ def on_connect(client, userdata, flags, rc):
         client.subscribe(i)
 
 
+
 def on_message(client, userdata, msg):
     message = str(msg.payload.decode("utf-8"))
     print("message:" + message)
-    send_command(message)
+    if message == 'calibrate':
+        calibration()
+    else:    
+        send_command(message)
 
-
+def calibration():
+    print('calibration')
+    clear_register()
+    time.sleep(motor_delay)
+    for dev in i2c_addr:
+        bus.write_byte_data(dev,i2c_register[1],255)
+    time.sleep(10)
+    clear_register()
+    
+    print('Calibration done..')
+    
 
 def send_command(message):
     comm, level, blind1, blind2 = bld.decode_command(message)
     list_of_blinds = bld.find_index(blind1+blind2)
-    reg_A=[]
-    reg_B=[]
+    
+    reg_A = []
+    reg_B=[] #read all registers to list
     for addr in i2c_addr:
         reg_A.append(read_data(addr, i2c_register[0]))
         reg_B.append(read_data(addr, i2c_register[1]))
 
     for i in list_of_blinds:
         if comm == 'f':
-            print(blinds[i].set_position(level))
-
+            blinds[i].set_position(level)
         elif comm == 't':
-            print(blinds[i].set_tilt(level))
+            blinds[i].set_tilt(level)
         elif comm == 's':
             blinds[i].stop()
             print(blinds[i].position)
-
 
     if comm != 's':
         if blinds[list_of_blinds[0]].movement == 'up':
@@ -136,21 +157,30 @@ def send_command(message):
             write_data(i2c_addr[0],i2c_register[0],bld.set_bit(reg_A[0],int(blind1,2)))
             write_data(i2c_addr[1],i2c_register[0],bld.set_bit(reg_A[1],int(blind2,2)))
         elif blinds[list_of_blinds[0]].movement == 'down':
-            print('down')
             write_data(i2c_addr[0],i2c_register[0],bld.clear_bit(reg_A[0],int(blind1,2)))
             write_data(i2c_addr[1],i2c_register[0],bld.clear_bit(reg_A[1],int(blind2,2)))
             time.sleep(motor_delay)
-            write_data(i2c_addr[0],i2c_register[1],bld.set_bit(reg_B,int(blind1,2)))
-            write_data(i2c_addr[1],i2c_register[1],bld.set_bit(reg_B,int(blind2,2)))
+            write_data(i2c_addr[0],i2c_register[1],bld.set_bit(reg_B[0],int(blind1,2)))
+            write_data(i2c_addr[1],i2c_register[1],bld.set_bit(reg_B[1],int(blind2,2)))
     else:
-        write_data(i2c_addr[0],i2c_register[0],bld.clear_bit(reg_A[0],int(blind1,2))) #clear particular bit
-        write_data(i2c_addr[1],i2c_register[0],bld.clear_bit(reg_A[1],int(blind2,2))) #clear particular bit
+        write_data(i2c_addr[0],i2c_register[0],bld.clear_bit(reg_A[0],int(blind1,2))) 
+        write_data(i2c_addr[1],i2c_register[0],bld.clear_bit(reg_A[1],int(blind2,2))) 
         write_data(i2c_addr[0],i2c_register[1],bld.clear_bit(reg_B[0],int(blind1,2)))
         write_data(i2c_addr[1],i2c_register[1],bld.clear_bit(reg_B[1],int(blind2,2)))
 
+#create object smbus
+bus = smbus.SMBus(1)
 
+##	MCP23017 - SET REGISTER AS OUTPUTS AND CLEAR ALL REGISTERS
+set_reg_as_output()
+clear_register()
+
+
+# run auto stop thread
 t1 = threading.Thread(target=blind_ctr)
-#t1.start()
+t1.start()
+
+#mqtt connection
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
